03f69a0f4677c2b904477fe8cbc031a1f84bd6b1
ViktarTromza <viktar.tromza@gmail.com>
2022-11-02 01:18:43 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
